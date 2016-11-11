97c771f32794b3e37c5d601a76ee4d7f946e02d1
ltaoo <litaowork@aliyun.com>
2016-11-11 22:05:29 +0800

    实现瀑布流布局

--- README.md
+++ README.md
@@ -14,3 +14,15 @@ npm run server
 # 打开另一个cmd窗口，开启开发服务器，将会自动打开浏览器
 npm run dev
 ```
+
+## 更新
+
+2016年11月11日 修改数据，增加 width 与 height ，使用第三方组件`vue-waterfall`，细微调整后实现瀑布流布局。调整如下：
+```javascript
+// lib/waterfall.vue
+// 177 行附近
+rect.height = meta.height * (options.fixedHeight ? 1 : width / meta.width)
+// 修改如下
+rect.height = (meta.height/meta.width)*width + 80
+```
+暂时的实现，等待之后优化。。
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="index">
-    <el-row 
-      :gutter="10" 
-      v-infinite-scroll="loadMore" 
-      infinite-scroll-disabled="busy" 
-      infinite-scroll-distance="10"
+    <waterfall
+      :align="align"
+      :line-gap="320"
+      :min-line-gap = "20"
+      :max-line-gap = "20"
+      :watch="images"
+      @reflowed="reflowed"
+      ref="waterfall"
     >
-      <el-col :span="8" v-for="img in images" class="img__item">
+      <!-- each component is wrapped by a waterfall slot -->
+      <waterfall-slot
+        v-for="(img, index) in images"
+        :width="img.width"
+        :height="img.height"
+        :order="index"
+        :key="img.id"
+        move-class="item-move"
+      >
         <el-card :body-style="{ padding: '0px', cursor: 'pointer'}">
           <img :src="img.original_url" class="image" @click="changeVisible(img)">
           <div style="padding: 14px;">
-            <span>{{img.web_name}}</span>
+            <span>{{index + img.web_name}}</span>
             <div class="bottom clearfix">
               <time class="time">{{ img.create_time }}</time>
             </div>
           </div>
         </el-card>
-      </el-col>
-    </el-row>
+      </waterfall-slot>
+    </waterfall>
     <div v-loading="busy" class="el-loading-demo"></div>
     <el-dialog v-model="dialogVisible" size="small">
       <img :src="currentImg.original_url" class="image">
@@ -26,6 +37,8 @@
 </template>
 
 <script>
+  import Waterfall from 'vue-waterfall/lib/waterfall'
+  import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
   export default {
     name: 'index',
     data () {
@@ -37,8 +50,16 @@
         currentImg: {
           original_url: ''
         },
-        busy: false
+        busy: false,
+        align: 'center'
       }
+    },
+    mounted () {
+      this.loadMore()
+    },
+    components: {
+      'waterfall': Waterfall,
+      'waterfall-slot': WaterfallSlot
     },
     methods: {
       changeVisible (img) {
@@ -55,7 +76,15 @@
               // 获取数据成功
               let oldAry = [...this.images]
               // this.images = [...oldAry, ...res.body]
+              res.body.img = res.body.img.map(item => {
+                console.log(item.width, item.height)
+                return Object.assign(item, {
+                  width: parseInt(item.width, 10),
+                  height: parseInt(item.height, 10)
+                })
+              })
               this.images = [...oldAry, ...res.body.img]
+              console.log(this.images)
               this.busy = false
               this.page += 1
             }
@@ -69,6 +98,14 @@
               showClose: true
             })
           })
+      },
+      shuffle: function () {
+        this.items.sort(function () {
+          return Math.random() - 0.5
+        })
+      },
+      reflowed: function () {
+        this.isBusy = false
       }
     }
   }
@@ -76,10 +113,42 @@
 
 <style>
   .index {
-    width: 980px;
+    width: 1180px;
     margin: 10px auto;
   }
   .img__item {
     margin-bottom: 10px;
+  }
+  .item-move {
+    transition: all .5s cubic-bezier(.55,0,.1,1);
+    -webkit-transition: all .5s cubic-bezier(.55,0,.1,1);
+  }
+  .el-card {
+    margin: 5px;
+  }
+  .item {
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    right: 5px;
+    bottom: 5px;
+    font-size: 1.2em;
+    color: rgb(0,158,107);
+  }
+  .item:after {
+    content: attr(index);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+  }
+  .wf-transition {
+    transition: opacity .3s ease;
+    -webkit-transition: opacity .3s ease;
+  }
+  .wf-enter {
+    opacity: 0;
   }
 </style>
